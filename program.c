38b642d5f4bd68464ef8db9abd6fa7f07b2e5197
uditkumar652 <62956423+uditkumar652@users.noreply.github.com>
2020-04-11 09:16:25 +0530

    avrage and min calculation

--- program.c
+++ program.c
@@ -7,10 +7,7 @@ void *thread1()
 {
 	int sum=0;
 	float avg=0;
-	printf("Enter Number of Elements::");
-	scanf("%d",&num);
-	for(iter=0;iter<num;iter++)
-	     sacnf("%d",&a[iiter]);
+
 	for(iter=0;iter<num;iter++)
 	{
 		sum+=a[iter];
@@ -19,26 +16,31 @@ void *thread1()
 	printf("The average value is:: %f",avg);
 }
 
-
-void main()
+void *thread2()
 {
-	pthread_t t1;
-	n=pthread_create(&t1,NULL,&thread1,NULL);
-	pthread_join(t1,NULL);
+	int min=a[0];
+	for(iter=1;iter<num;iter++)
+	{
+		if(min>a[iter])
+		    min=a[iter];
+	}
+	printf("\nThe Minimum value is:: %d",min);
 }
 
 
+int main()
+{
+	printf("Enter Number of Elements::");
+	scanf("%d",&num);
+	for(iter=0;iter<num;iter++)
+	     sacnf("%d",&a[iter]);
+	
+	pthread_t t1,t2,t3;
+	n=pthread_create(&t1,NULL,&thread1,NULL);
+	pthread_join(t1,NULL);
 
+	n=pthread_create(&t2,NULL,&thread2,NULL);
+	pthread_join(t2,NULL);
 
-
-
-
-
-
-
-
-
-
-
-
-
+	return 0;
+}
